65d9a4daa55beecd7e1bbce0fdaccc35385760a6
adelneo <adelinception@gmail.com>
2023-12-13 14:06:15 -0500

    main.py

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import requests
 
-# The API endpoint
+
 url = "https://jsonplaceholder.typicode.com/users"
 
-# A GET request to the API
+
 response = requests.get(url)
 
-# Check if the request was successful (status code 200)
+
 import requests
 
-# The API endpoint
+
 url = "https://jsonplaceholder.typicode.com/users"
 
-# A GET request to the API
+
 response = requests.get(url)
 
-# Check if the request was successful (status code 200)
+
 if response.status_code == 200:
-    # Parse the JSON response
+
     users = response.json()
 
-    # Sort users by first name alphabetically
+
     sorted_users = sorted(users, key=lambda x: x['name'].split()[0])
 
-    # Iterate through each sorted user and print relevant information
+
     print("Here's a list of the users sorted alphabetically by their first name : ")
     print("-----")
     for user in sorted_users:
@@ -39,8 +36,8 @@ if response.status_code == 200:
         print("-----")  # Separator between users
 
 else:
-    # Print an error message if the request was not successful
+
     print(f"Failed to retrieve user data. Status code: {response.status_code}")
 
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
